bdc373c1e1cb4134f4742e4330c4e8f5a47fd909
Jet Lawrence P. Doctor <jetlawrence.doctor@gmail.com>
2016-10-10 05:29:34 +0800

    Main Files

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Register from "./screens/register";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Register/>
+      </div>
+    );
+  }
+}
